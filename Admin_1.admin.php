dfaf2b5219f37201e56330be5ff6a0fb890ee5ca
Mathieu Kabo Junior <182039913+lebonbamaritain@users.noreply.github.com>
2025-02-24 10:28:20 +0000

    Add files via upload

---
+++ Admin_1.admin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 
+ * 
+ * 
+ */ 
+class Admin_1 {
+   
+    
+    
+    public function __construct() {
+        
+        echo "This is constructor of class Admin 1 <BR>";
+
+    }
+
+
+}
